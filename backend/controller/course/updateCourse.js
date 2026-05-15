ff8906345efd8ef5823eb7552c9542d58bc81adc
surya2prakash <sv251171@gmail.com>
2026-05-15 15:33:06 +0530

    update Course

---
+++ backend/controller/course/updateCourse.js
@@ -0,0 +1,65 @@
+const Course = require("../../model/course");
+const uploadToCloudinary = require("../../utility/uploadToCloudinary");
+
+require("dotenv").config();
+
+
+
+exports.editCourse = async(req,res) =>{
+       try{
+
+          const {courseId} = req.body ;
+
+        //   updated 
+          const updates = req.body ;
+
+          const isCourseExist = await Course.findById(courseId);
+
+          if(!isCourseExist){
+              return res.status(404).json({
+                  success:false,
+                  message:"Course Not Found."
+              })
+          };
+            
+          if(req.files || req.files.thumbnailImage){
+                const thumbnail =  req.files.thumbnailImage;
+
+              const result =  await uploadToCloudinary(thumbnail,process.env.THUMBNAIL_FOLDER);
+
+                isCourseExist.thumbnail = result?.secure_url ;
+          }
+
+
+
+
+        //   updated the field which present in updates -->
+
+          for(const key in updates){
+                 
+                  if(updates.hasOwnProperty(key)){
+                        if(key === 'tag' || key === 'instructions'){
+                              isCourseExist[key] = JSON.parse(updates[key])
+                        }else{
+                             isCourseExist[key] = updates[key];
+                        }
+                  }
+          }
+         await isCourseExist.save();
+
+         const updatedCourse = await Course.findById(isCourseExist?._id);
+
+         return res.status(200).json({
+                success:true,
+                message:"Course Updated.",
+                data:updatedCourse
+         })
+       }catch(err){
+             console.error(err);
+
+             return res.status(500).json({
+                  success:false,
+                  message:"Internal Server Error."
+             })
+       }
+}
