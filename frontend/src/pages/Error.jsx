c935e328df7a3ef36dbcd21c4b82cb5c2c0a5fae
surya2prakash <sv251171@gmail.com>
2026-06-12 23:48:28 +0530

    Error page

---
+++ frontend/src/pages/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <div className='flex flex-1 justify-center items-center text-white text-3xl'>
+          Error 404 - Page Not Found
+    </div>
+  )
+}
