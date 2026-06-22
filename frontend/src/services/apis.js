65f787852ea0b1a7b632872a652f32164561aef1
surya2prakash <sv251171@gmail.com>
2026-06-22 20:17:18 +0530

    API ->

---
+++ frontend/src/services/apis.js
@@ -0,0 +1,16 @@
+const BASE_URL =import.meta.env.VITE_BASE_URL;
+
+
+
+
+// CATEGORY API ---->
+export const categories = {
+         CATEGORIES_API :BASE_URL + "/category"
+}
+
+//LOGIN API -->
+export const endPoints ={
+     LOGIN_API :BASE_URL +"/login",
+      SIGNUP_API : BASE_URL +"/signup"
+};
+
