fe87e2846f33b5df557b5f28efd14aebc0ab01f6
surya2prakash <sv251171@gmail.com>
2026-06-22 20:16:28 +0530

    api connector -> axios setup

---
+++ frontend/src/services/apiConnector.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const apiInstance = axios.create({});
+
+export const apiConnector = (method,url , bodyData, headers, params)=>{
+        console.log(method,url,bodyData)
+        return (
+              apiInstance({
+                   method:`${method}`,
+                   url:`${url}`,
+                   data:bodyData? bodyData : null,
+                   headers:headers ? headers :null ,
+                   params:params ? params :null
+              })
+        )
+};
