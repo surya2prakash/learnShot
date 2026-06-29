25e8eb6ec6126a1201c60d252f6a395bc6efe4b0
surya2prakash <sv251171@gmail.com>
2026-06-29 22:24:55 +0530

    Open Route setup

---
+++ frontend/src/components/core/Auth/OpenRoute.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Navigate } from 'react-router-dom';
+
+export default function OpenRoute({children}) {
+    
+    const {token} = useSelector(state => state.auth);
+
+    if(token === null){
+          return children
+    }else{
+          <Navigate to="/dashboard/my-profile"/>
+    }
+}
