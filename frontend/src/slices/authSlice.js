4730b29c8df29ddb448356c0925595ac8ccf1864
surya2prakash <sv251171@gmail.com>
2026-07-08 21:54:35 +0530

    extra reducer is added to auth slice

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -1,10 +1,12 @@
  import { createSlice } from "@reduxjs/toolkit";
+import { login, logout, signup } from "../services/operations/authApi";
 
 
  const initialState = {
        signupData:null,
        loading:false,
-       token:localStorage.getItem("token") ? (JSON.parse(localStorage.getItem("token"))) :null
+       token:localStorage.getItem("token") ? (JSON.parse(localStorage.getItem("token"))) :null,
+       error:{}
  };
 
  const authSlice = createSlice({
@@ -14,7 +16,55 @@
          setToken (state,value){
              state.token = value.payload
          }
+     },
+     extraReducers:(builder)=>{
+           builder
+           .addCase(login.pending,(state)=>{
+               state.loading = true
+           })
+
+           .addCase(login.fulfilled ,(state,action)=>{
+                          console.log("action->",action);
+                     state.loading = false;
+                     state.token = localStorage.setItem("token",JSON.stringify(action.payload.token)) ;          
+           })
+           .addCase(login.rejected,(state,action)=>{
+                  state.loading = false;
+                  state.error =action.payload;
+                  
+           })
+
+           .addCase(signup.pending,(state)=>{
+                 state.loading = true;
+           })
+
+           .addCase(signup.fulfilled,(state,action)=>{
+                   state.loading = false
+                   state.signupData = action.payload.user
+           })
+
+           .addCase(signup.rejected,(state,action)=>{
+                 state.error = action.payload 
+           })
+
+           .addCase(logout.pending,(state)=>{
+                  state.loading = true
+                   
+                   localStorage.removeItem("token");
+                   state.token=null;
+           })
+
+           .addCase(logout.fulfilled,(state,action)=>{
+                  state.loading =false;
+                  state.signupData =null;
+           })
+
+           .addCase(logout.rejected,(state,action)=>{
+                state.error = action.error
+           })
+           
      }
+
  });
 
  export const {setToken} = authSlice.actions 
