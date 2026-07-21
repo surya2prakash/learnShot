b3b2a6e826c7e2d49c56f8fad55f44fd48618e3a
surya2prakash <sv251171@gmail.com>
2026-07-21 21:18:13 +0530

    confirmationModel component

---
+++ frontend/src/components/Common/ConfirmationModal.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import IconBtn from './IconBtn'
+
+export default function ConfirmationModal({modalData}) {
+    console.log(modalData);
+  return (
+    <div className='fixed inset-0 z-[1000] !mt-0 grid place-items-center overflow-auto bg-white bg-opacity-10 backdrop-blur-sm'>
+        <div className='w-11/12 max-w-[350px] rounded-lg border border-richblack-400 bg-richblack-800 p-6'>
+            <p className='text-2xl font-semibold text-richblack-5'>{modalData?.text1}</p>
+            <p className='mt-3 mb-5 leading-6 text-richblack-200'>{modalData?.text2}</p>
+            <div className='flex items-center gap-x-4'>
+                <IconBtn text={modalData?.btnText1} onClick={modalData?.btn1Handler}/>
+                <button onClick={modalData?.btn2Handler} className='cursor-pointer rounded-md bg-richblack-200 py-[8px] px-[20px] text-richblack-900'>
+                     {
+                        modalData?.btnText2
+                     }
+                </button>
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
