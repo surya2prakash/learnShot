a8f8c7dc9abacf630308e341e9e952fee0fd70a0
surya2prakash <sv251171@gmail.com>
2026-07-22 21:01:04 +0530

    Add course index.jsx main file

---
+++ frontend/src/components/core/Dashboard/AddCourse/index.jsx
@@ -0,0 +1,37 @@
+import RenderSteps from "./RenderSteps";
+
+
+export default function AddCourse (){
+      return(
+        <div>
+            <div>
+                <h1 className="mb-14 text-3xl font-medium text-richblack-5">Add Course</h1>
+                <div>
+                    <RenderSteps/>
+                </div>
+            </div>
+            {/* course upload tips ---> */}
+
+            <div>
+               <p className="mb-8 text-lg text-richblack-5">⚡ Course Upload Tips</p>
+                <ul className="ml-5 list-item list-disc space-y-4 text-xs text-richblack-5">
+                  <li>Set the Course Price option or make it free.</li>
+                  <li>Standard size for the course thumbnail is 1024x576.</li>
+                  <li>Video section controls the course overview video.</li>
+                 <li>Course Builder is where you create & organize a course.</li>
+                  <li>
+              Add Topics in the Course Builder section to create lessons,
+              quizzes, and assignments.
+                </li>
+                 <li>
+              Information from the Additional Data section shows up on the
+              course single page.
+                </li>
+               <li>Make Announcements to notify any important</li>
+               <li>Notes to all enrolled students at once.</li>
+          </ul>
+
+            </div>
+        </div>
+      )
+}
