50f4b4a305d0e7f46834ae56f3fe964ba7f45075
surya2prakash <sv251171@gmail.com>
2026-07-28 22:50:31 +0530

    withCredentials add for cookies

--- frontend/src/services/apiConnector.js
+++ frontend/src/services/apiConnector.js
@@ -10,7 +10,8 @@ export const apiConnector = (method,url , bodyData, headers, params)=>{
                    url:`${url}`,
                    data:bodyData? bodyData : null,
                    headers:headers ? headers :null ,
-                   params:params ? params :null
+                   params:params ? params :null,
+                   withCredentials:true
               })
         )
 };
