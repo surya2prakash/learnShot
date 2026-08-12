11c30472220f61f463ff7acdcfc8d72551852964
surya2prakash <sv251171@gmail.com>
2026-08-12 19:12:07 +0530

    getfullCourseDetails added

--- backend/controller/course/getCourse.js
+++ backend/controller/course/getCourse.js
@@ -1,4 +1,6 @@
 const Course = require("../../model/course");
+const courseProgress = require("../../model/courseProgress");
+const convertSecToDuration = require("../../utility/secondToDuration/secToDuration")
 
 
 
@@ -111,4 +113,82 @@ exports.getInstructorCourses = async(req,res) =>{
                    message:"Internal Server Error."
              })
         }
+}
+
+
+
+exports.getFullCourseDetails = async(req,res) =>{
+         try{
+
+            const {courseId} = req.body ;
+
+            const userId = req.user.id ;
+
+
+            const courseDetails = await Course.findOne({_id:courseId}).populate({
+                  path:"instructorId",
+                  model:"User",
+                  populate:{
+                        path:"profileId",
+                        model:"Profile" 
+                  }
+            }).populate({
+                  path:"categoryId",
+                  model:"Category"
+            }).populate({
+                  path:"ratingAndReviews",
+                  model:"RatingAndReviews"
+            }).populate({
+                   path:"sections",
+                   model:"Section",
+                   populate:{
+                        path:"subSectionsId",
+                        model:"SubSection"
+                   }
+            }).exec();
+
+            const courseProgressCount = await courseProgress.findOne({
+                     courseId:courseId,
+                     userId:userId 
+            });
+
+            if(!courseDetails){
+               
+                  return res.status(404).json({
+                         success:false,
+                         message:"Could not find the course with id ."
+                  })
+            };
+
+            let totalDurationInSeconds = 0 ;
+
+            courseDetails.sections.forEach((content)=>{
+                   content.subSectionsId.forEach((subSection)=>{
+                       const timeDurationInSeconds = parseInt(subSection?.timeDuration) ;
+
+                          totalDurationInSeconds += timeDurationInSeconds
+                   })
+            })
+
+                const totalDuration = convertSecToDuration(totalDurationInSeconds);
+
+                 
+
+                return res.status(200).json({
+                    success:true,
+                    message:"Course All details",
+                    data:{
+                           course:courseDetails,
+                           totalDuration:totalDuration,
+                           completedVideos:courseProgressCount?.completedVideos ? courseProgressCount?.completedVideos : []
+                    }
+                })
+         }catch(error){
+               console.error(error);
+
+               return res.status(500).json({
+                    success:false,
+                    message:"Internal Server Error"
+               })
+         }
 }
