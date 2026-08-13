ba3d70b0815c844296e39b76edafb45f3f83fd23
surya2prakash <sv251171@gmail.com>
2026-08-13 14:46:12 +0530

    Routes are added to the main file

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const cloudinary = require("./configFiles/cloudinary");
 const database = require("./configFiles/database");
+const fileUpload = require("express-fileupload");
+//const client = require("./configFiles/redisClient");
+const userRoutes = require("./routes/user");
+const courseRoutes = require("./routes/course");
+const paymentRoute = require("./routes/payment");
+const profileRoute = require("./routes/profile");
+
+
+const cookieparser = require("cookie-parser")
+
 
 // express ---->
 const app = express();
@@ -19,12 +29,39 @@ const PORT = process.env.PORT || 8000
 database();
 cloudinary();
 
+app.use(fileUpload({
+      useTempFiles:true,
+      tempFileDir:"/tmp/"
+}));
 
-// listen --->
-app.listen(PORT,()=>{
-      console.log(`App is listen at ${PORT} 🌎`);
-});
+app.use(cookieparser());
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    methods:["POST","GET","DELETE","PATCH"],
+    allowedHeaders:["Authorization","Content-Type","Accept"],
+    credentials:true
+}))
 
 app.get("/",(req,res)=>{
        res.send(`<div>Welcome to backend.</div>`)
 });
+
+app.use("/api/v1",userRoutes);
+app.use("/api/v1",courseRoutes);
+app.use("/api/v1",paymentRoute);
+app.use("/api/v1",profileRoute);
+
+// listen --->
+
+async function startServer() {
+      //  await client.connect();
+      app.listen(PORT,()=>{
+      console.log(`App is listen at ${PORT} 🌎`);
+});
+};
+
+startServer();
+
+
+
